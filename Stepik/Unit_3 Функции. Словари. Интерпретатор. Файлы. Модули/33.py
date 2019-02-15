79aa5c7f061b313a0cec2e99397f13394516e48c
hyperprism <dmitrii.abanin@yandex.ru>
2019-02-15 21:46:26 +0300

    omg

--- Stepik/Unit_3 Функции. Словари. Интерпретатор. Файлы. Модули/33.py
+++ Stepik/Unit_3 Функции. Словари. Интерпретатор. Файлы. Модули/33.py
@@ -1,3 +1,11 @@
 import requests
-r = requests.get('https://stepic.org/media/attachments/course67/3.6.2/050.txt')
+
+download_link = 'https://stepic.org/media/attachments/course67/3.6.3/'
+filename = '213837.txt'
+
+while filename:
+    print(filename)
+    r = requests.get(download_link + filename)
+    filename = None if r.text.startswith('We') else r.text
+
 print(r.text)
